3016aa6b807246918b49ce323cdbf1665d5334bf
leejongyun <73763976+jayyaj1224@users.noreply.github.com>
2021-12-14 12:18:25 +0900

    Renewal_003

--- Dangun Legend/[Cave]/Cell/CaveScrollViewCell.swift
+++ Dangun Legend/[Cave]/Cell/CaveScrollViewCell.swift
@@ -10,4 +10,96 @@ import UIKit
 
 class CaveScrollViewCell: UIView {
     
+    var collectionView: UICollectionView!
+    var characterView: UIView!
+    var caveExitView: UIView!
+    
+    var asdfjkl = Array(0...100)
+    
+    init() {
+        super.init(frame: CGRect(x: 0, y: 0, width: 1300, height: 200))
+        self.setupCollectionView()
+        
+        self.setupCharacterView()
+        self.setupCaveExitView()
+    }
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    private func setupCollectionView() {
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout.init()
+        layout.scrollDirection = .horizontal
+        let collectionView = UICollectionView.init(frame: CGRect(x: 0, y: 0, width: 1300, height: 200), collectionViewLayout: layout)
+        collectionView.register(StepCell.self, forCellWithReuseIdentifier: "StepCell")
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .clear
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        self.addSubview(collectionView)
+        collectionView.snp.makeConstraints { make in
+            make.size.equalToSuperview()
+            make.center.equalToSuperview()
+        }
+    }
+    
+    private func setupCharacterView() {
+        let view = UIView()
+        view.backgroundColor = .brown
+        view.alpha = 0.3
+        view.layer.cornerRadius = 50
+        self.addSubview(view)
+        view.snp.makeConstraints { make in
+            make.top.bottom.equalToSuperview()
+            make.leading.equalToSuperview().offset(20)
+            make.width.equalTo(100)
+        }
+        self.caveExitView = view
+    }
+    
+    private func setupCaveExitView() {
+        let view = UIView()
+        view.backgroundColor = .lightGray
+        view.layer.cornerRadius = 50
+        self.addSubview(view)
+        view.snp.makeConstraints { make in
+            make.top.bottom.trailing.equalToSuperview()
+            make.width.equalTo(100)
+        }
+        self.caveExitView = view
+    }
+}
+
+extension CaveScrollViewCell: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        self.asdfjkl.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "StepCell", for: indexPath) as? StepCell else {
+            return UICollectionViewCell()
+        }
+        cell.label.text = "\(indexPath.row)"
+        return cell
+    }
+}
+
+extension CaveScrollViewCell: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 40, height: 200)
+    }
+}
+
+class StepCell: UICollectionViewCell {
+    
+    var label: UILabel!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.label = UILabel()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
--- Dangun Legend/[Cave]/View/CaveViewController.swift
+++ Dangun Legend/[Cave]/View/CaveViewController.swift
@@ -22,8 +22,7 @@ class CaveViewController: UIViewController {
         
         self.setupMainScrollView()
         
-//        self.setupMainStackView()
-        self.setupContentStackViewType1()
+        self.setupMainStackView()
         
         self.setupAddGoalButton()
     }
@@ -54,7 +53,33 @@ class CaveViewController: UIViewController {
     }
     
     private func setupMainStackView() {
+        let stackView = UIStackView()
         
+        [1,2,3,4,5]
+            .map { _ in
+                return CaveScrollViewCell()
+            }
+            .forEach { view in
+                stackView.addArrangedSubview(view)
+                view.layer.cornerRadius = 100
+                view.snp.makeConstraints { make in
+                    make.height.equalTo(200)
+                    make.width.equalTo(self.view.frame.width)
+                }
+            }
+        stackView.axis = .vertical
+        stackView.alignment = .leading
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 30
+        
+        self.mainScrollView.addSubview(stackView)
+        stackView.snp.makeConstraints { make in
+            make.top.bottom.equalToSuperview()
+            make.trailing.equalToSuperview()
+            make.leading.equalToSuperview()
+            make.width.equalToSuperview()
+        }
+        self.mainStackView = stackView
     }
     
     private func setupAddGoalButton() {
@@ -72,61 +97,61 @@ class CaveViewController: UIViewController {
         self.addGoalButton = button
     }
     
-    private func colourViews() -> [UIView] {
-        var colours: [UIColor] = [.green, .red, .blue, .yellow, .orange, .brown]
-        
-        for _ in 0...3 {
-            colours.append(contentsOf: colours)
-        }
-            
-        let coulourViews = colours
-            .map { colour -> UIView in
-                let view = UIView()
-                view.backgroundColor = colour
-                
-                let label = UILabel()
-                if #available(iOS 14.0, *) {
-                    label.text = colour.accessibilityName
-                } else {
-                    label.text = colour.description
-                }
-                label.font = .fontSFProDisplay(size: 20, family: .Medium)
-                view.addSubview(label)
-                label.snp.makeConstraints { make in
-                    make.center.equalToSuperview()
-                }
-                return view
-            }
-        return coulourViews
-    }
+//    private func colourViews() -> [UIView] {
+//        var colours: [UIColor] = [.green, .red, .blue, .yellow, .orange, .brown]
+//
+//        for _ in 0...3 {
+//            colours.append(contentsOf: colours)
+//        }
+//
+//        let coulourViews = colours
+//            .map { colour -> UIView in
+//                let view = UIView()
+//                view.backgroundColor = colour
+//
+//                let label = UILabel()
+//                if #available(iOS 14.0, *) {
+//                    label.text = colour.accessibilityName
+//                } else {
+//                    label.text = colour.description
+//                }
+//                label.font = .fontSFProDisplay(size: 20, family: .Medium)
+//                view.addSubview(label)
+//                label.snp.makeConstraints { make in
+//                    make.center.equalToSuperview()
+//                }
+//                return view
+//            }
+//        return coulourViews
+//    }
     
-    private func setupContentStackViewType1() {
-        let stackView = UIStackView()
-        
-        self.colourViews()
-            .forEach { view in
-                stackView.addArrangedSubview(view)
-                view.layer.cornerRadius = 100
-                view.alpha = 0.2
-                view.snp.makeConstraints { make in
-                    make.height.equalTo(200)
-                    make.width.equalTo(200)
-                }
-            }
-        stackView.axis = .vertical
-        stackView.alignment = .leading
-        stackView.distribution = .equalSpacing
-        stackView.spacing = 30
-        
-        self.mainScrollView.addSubview(stackView)
-        stackView.snp.makeConstraints { make in
-            make.top.bottom.equalToSuperview()
-            make.trailing.equalToSuperview()
-            make.leading.equalToSuperview()
-            make.width.equalToSuperview()
-        }
-        self.mainStackView = stackView
-    }
+//    private func setupContentStackViewType1() {
+//        let stackView = UIStackView()
+//
+//        self.colourViews()
+//            .forEach { view in
+//                stackView.addArrangedSubview(view)
+//                view.layer.cornerRadius = 100
+//                view.alpha = 0.2
+//                view.snp.makeConstraints { make in
+//                    make.height.equalTo(200)
+//                    make.width.equalTo(200)
+//                }
+//            }
+//        stackView.axis = .vertical
+//        stackView.alignment = .leading
+//        stackView.distribution = .equalSpacing
+//        stackView.spacing = 30
+//
+//        self.mainScrollView.addSubview(stackView)
+//        stackView.snp.makeConstraints { make in
+//            make.top.bottom.equalToSuperview()
+//            make.trailing.equalToSuperview()
+//            make.leading.equalToSuperview()
+//            make.width.equalToSuperview()
+//        }
+//        self.mainStackView = stackView
+//    }
     
     
     
